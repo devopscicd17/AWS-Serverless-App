ed0bc51a507e628ccee98f166ca533998eceb111
devopscicd17 <86373586+devopscicd17@users.noreply.github.com>
2025-08-04 23:24:41 +0530

    Update app.py

--- lambda/app.py
+++ lambda/app.py
@@ -1,9 +1,5 @@
-import json
-
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "Hello from Dockerized Lambda!"
-        }),
+        "body": "Hello from Lambda container image!"
     }
